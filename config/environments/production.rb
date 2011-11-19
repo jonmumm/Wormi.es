762e89aa06825d76caa1feddff0b45b4e79f85b2
Jonathan Mumm <mumm@tokbox.com>
2011-11-19 10:20:13 -0800

    Set up for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Wormies::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile << "*.js"
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
